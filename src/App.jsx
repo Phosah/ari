7ca21ce2b4dd29bb2e3a881979264a78bcde3275
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2022-08-30 06:48:34 -0700

    Clean up on starter files

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import Illustrations from './components/Illustrations'
 import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App bg-black text-white max-w-7xl mx-auto">
-      <Illustrations />
+    <div className="min-h-screen bg-black text-white">
       <Navbar />
-      <h1>Ari</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <Illustrations />
     </div>
   )
 }
--- src/components/Illustrations.jsx
+++ src/components/Illustrations.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Illustrations = () => {
   return (
-    <div>Illustrations</div>
+    <main>
+      <div>Illustrations</div>
+      <h4>PROJECT 1/4</h4>
+      <h2>Rafiki - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit vitae, orci dictum amet...</h2>
+    </main>
   )
 }
 
