1008c0ce33b3d7f414364bf484eba90325c68375
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2022-08-30 10:57:30 -0700

    Update to Navbar

--- src/components/Illustrations.jsx
+++ src/components/Illustrations.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Illustrations = () => {
   return (
     <main className='max-w-7xl mx-auto my-20'>
-      <div className='my-12 text-4xl'>Illustrations</div>
+      <div className='my-12 text-7xl'>Illustrations</div>
       <h4 className='mb-4 text-sm'>PROJECT 1/4</h4>
       <h2 className='text-4xl'>Rafiki - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit vitae, orci dictum amet...</h2>
     </main>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import logo from '../assets/logo.svg';
+import menu from '../assets/menu.svg';
 
 export default function Navbar() {
     return (
-        <div className="max-w-7xl mx-auto my-10 py-10">This is the Navbar</div>
+        <nav className="flex items-center justify-between max-w-7xl mx-auto my-10 py-10">
+            <div><img src={logo} alt="Menu logo" /></div>
+            <div><img src={menu} alt="Menu logo" /></div>
+        </nav>
     );
 }
