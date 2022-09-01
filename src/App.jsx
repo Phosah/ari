682a9be2b469674b2e76b13ebe917c9caf717773
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2022-09-01 11:36:24 -0700

    Fixes to font style

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Main from './components/Main'
 import Footer from './components/Footer'
-import './fonts/Larken-Bold.ttf'
 
 function App() {
 
