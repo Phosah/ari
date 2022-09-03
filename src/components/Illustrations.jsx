7efcc6c7fe93b8865626539e59262e0c233db908
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2022-09-02 19:26:19 -0700

    Update to Illustration component

--- src/components/Illustrations.jsx
+++ src/components/Illustrations.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import illustrations1 from "../assets/illustrations-1.png"
 
 const Illustrations = () => {
   return (
     <main className='max-w-7xl mx-auto my-20'>
       <div className='my-12 text-7xl'>Illustrations</div>
       <h4 className='mb-4 text-sm'>PROJECT 1/4</h4>
-      <h2 className='text-4xl'>Rafiki - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit vitae, orci dictum amet...</h2>
+      <h2 className='mb-4 text-4xl'>Rafiki - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit vitae, orci dictum amet...</h2>
+      <img src={illustrations1} alt="Illustrations" />
     </main>
   )
 }
