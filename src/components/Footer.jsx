e143377d5f332449df536178b496aa96e7a3f25a
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2022-09-06 05:14:51 -0700

    Addition to Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,20 @@ import React from "react";
 
 export default function Footer() {
     return (
-        <main className="footer h-96 bg-yellow-200 text-gray-600">This is the footer</main>
+        <main className="footer min-h-96">
+            <div className="max-w-7xl mx-auto ">
+                <div className="flex items-center space-x-6 py-12">
+                    <div className="w-48 h-48 border-2 border-white rounded-full flex items-center justify-center"><a href="">Email</a></div>
+                    <div className="w-48 h-48 border-2 border-white rounded-full flex items-center justify-center"><a href="">Calendly</a></div>
+                </div>
+                <div className="flex space-x-4 py-20">
+                    <a href="">Twitter</a>
+                    <a href="">Dribble</a>
+                    <a href="">Behance</a>
+                    <a href="">LinkedIn</a>
+                    <a href="">Medium</a>
+                </div>
+            </div>
+        </main>
     )
 }
