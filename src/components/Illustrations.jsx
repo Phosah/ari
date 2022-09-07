b69dc6c4db22c974a3e37db918a8d0e7ddae4719
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2022-09-07 08:56:32 -0700

    Update to Projects page

--- src/components/Illustrations.jsx
+++ src/components/Illustrations.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Illustrations = props => {
   return (
-    <main className='max-w-7xl mx-auto my-20'>
-      <h4 className='mb-4 text-sm'>{props.project}</h4>
-      <h2 className='mb-4 text-4xl'>{props.description}</h2>
+    <main className='max-w-7xl mx-auto mb-20'>
+      <h4 className='mb-6 text-brand-gray-1 font-light'>{props.project}</h4>
+      <h2 className='mb-12 text-4xl'><span className={props.titleColor}>{props.projectTitle}</span> {props.description}</h2>
       <img src={props.img} alt="Illustrations" />
     </main>
   )
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,16 +6,34 @@ import illustration2 from "../assets/illustrations-2.png";
 export default function Projects () {
     return (
         <div className="max-w-7xl mx-auto">
-            <h1 className="larken-bold mb-8 text-7xl">Illustrations</h1>
+            <h1 className="py-20 text-7xl">Illustrations</h1>
             <Illustrations 
                 project='PROJECT 1/4' 
-                description="Rafiki - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit vitae, orci dictum amet..." 
+                projectTitle='Rafiki -'
+                description=" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit vitae, orci dictum amet..." 
                 img={illustration1}
+                titleColor="text-brand-green-1"
             />
             <Illustrations 
                 project="PROJECT 2/4" 
-                description="Expressions - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit vitae, orci..." 
+                projectTitle="Expressions -"
+                description=" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit vitae, orci..." 
                 img={illustration2}
+                titleColor="text-[#3B61FF]"
+            />
+            <Illustrations 
+                project='PROJECT 3/4' 
+                projectTitle='Rafiki -'
+                description=" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit vitae, orci dictum amet..." 
+                img={illustration1}
+                titleColor="text-[#EEA03C]"
+            />
+            <Illustrations 
+                project="PROJECT 4/4" 
+                projectTitle="Expressions -"
+                description=" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit vitae, orci..." 
+                img={illustration2}
+                titleColor="text-[#3B61FF]"
             />
         </div>
     )
