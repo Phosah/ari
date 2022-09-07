87b55cb4ba19feee04f80bd9478a6d9b08d621d4
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2022-09-06 17:02:14 -0700

    Improvement of home styles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import ari from "../assets/ari.png";
+import brush from "../assets/paint-brush.svg";
+import happyface from "../assets/happy-face.svg";
 
 export default function Home () {
     return (
         <main className="max-w-7xl mx-auto px-6">
             <div className="py-20">
-                <h1 className="text-7xl">Visual Designer and Art Director Creating Delightful Experieces</h1>
+                <h1 className="text-7xl italic">Visual Designer 
+                    <span className="text-brand-gray-1 not-italic"> and</span> Art Director 
+                    <span><img className="inline-block" src={brush} alt="Paint brush" /></span> 
+                    <span className="text-brand-gray-1 not-italic">Creating Delightful Experiences</span>
+                </h1>
+                <div className="flex items-center justify-center py-10"><img src={happyface} alt="happy face smiley" /></div>
             </div>
             <div className="flex items-center space-x-4 py-20">
                 <div><img src={ari} alt="Ari photo" /></div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,15 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          gray: {
+            1: "#818181"
+          }
+        }
+      }
+    },
   },
   plugins: [],
 }
