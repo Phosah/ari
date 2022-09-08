9a62373601d334cc2f3a47549eb7708fbffbe827
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2022-09-08 06:09:19 -0700

    Update to About page styles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,16 +8,20 @@ function About () {
     return (
         <main className="max-w-7xl mx-auto px-6">
             <Navbar />
-            <h1>About Me</h1>
-            <h3 className="mb-12">BRIDGING THE GAP BETWEEN PEOPLE AND PRODUCTS</h3>
-            <div className="flex items-center justify-between my-20">
+            <div className="py-20 text-center">
+                <h1 className="mb-4 text-7xl">About Me</h1>
+                <h3 className="mb-12">BRIDGING THE GAP BETWEEN PEOPLE AND PRODUCTS</h3>
+            </div>
+            <div className="flex items-center justify-between space-x-4 my-20">
                 <div><img src={person1} alt="Person" /></div>
                 <div><img src={person2} alt="Person" /></div>
                 <div><img src={person3} alt="Person" /></div>
                 <div><img src={person4} alt="Person" /></div>
             </div>
-            <div className="flex space-x-8">
-                <h2 className="flex-1">Who is Ari</h2>
+            <div>
+
+            <div className="flex space-x-8 px-20 py-20">
+                <h2 className="flex-1 text-6xl">Who is Ari</h2>
                 <div  className="flex-1">
                     <p className="mb-6">I’m a Product Designer, Writer, and Public Speaker passionate about entrepreneurial thinking, being an infinite player, behavioural psychology, and bridging the gap between business and design.</p>
                     <p className="mb-6">I currently lead Product & Design at Paystack (Stripe subsidiary) where I work with the teams to help build the best payment experiences for high-growth businesses transacting across Africa and the rest of the world.</p>
@@ -26,9 +30,9 @@ function About () {
                     <p className="mb-6">Overall, I love adventure, learning new perspectives from diverse people, and enriching my mind with different views & experiences. So when I’m not creating some on my laptop, I'm also a photographer; slightly obsessed with documenting experiences and can usually be found outdoors exploring something new.</p>
                 </div>
             </div>
-            <div className="flex justify-between space-x-8 my-20">
-                <h2>Hobbies</h2>
-                <ul>
+            <div className="flex justify-between space-x-8 px-20 py-20 my-20">
+                <h2 className="flex-1 text-7xl">Hobbies</h2>
+                <ul className="flex-1 pl-6 list-disc">
                     <li>Swimming</li>
                     <li>Going to the movies</li>
                     <li>Reading</li>
@@ -37,33 +41,34 @@ function About () {
                     <li>Enim semper </li>
                 </ul>
             </div>
-            <div className="flex justify-between space-x-8 my-20">
-                <h2 className="flex-1">Experience</h2>
+            <div className="flex justify-between space-x-8 px-20 py-20 mt-20">
+                <h2 className="flex-1 text-7xl">Experience</h2>
                 <div className="flex-1">
-                    <div>
-                        <h4>Paystack, Dec 2020 — Present</h4>
-                        <h5>Senior Product Designer | EMEA</h5>
+                    <div className="mb-10">
+                        <h4 className="mb-2">Paystack, Dec 2020 — Present</h4>
+                        <h5 className="text-brand-gray-2">Senior Product Designer | EMEA</h5>
                     </div>
-                    <div>
-                        <h4>Kingdom, Jan 2022 — May 2022</h4>
-                        <h4>CPO & Co-founder | San Francisco, USA</h4>
+                    <div className="mb-10">
+                        <h4 className="mb-2">Kingdom, Jan 2022 — May 2022</h4>
+                        <h5 className="text-brand-gray-2">CPO & Co-founder | San Francisco, USA</h5>
                     </div>
                     
-                    <div>
-                        <h4>Base Capital, Dec 2020 — Apr 2021</h4>
-                        <h4>UX Design Lead | New York, USA</h4>
+                    <div className="mb-10">
+                        <h4 className="mb-2">Base Capital, Dec 2020 — Apr 2021</h4>
+                        <h5 className="text-brand-gray-2">UX Design Lead | New York, USA</h5>
                     </div>
 
-                    <div>
-                        <h4>Pull Up, Nov 2020 — Mar 2021</h4>
-                        <h5>Lead Product Designer | Lagos, Nigeria</h5>
+                    <div className="mb-10">
+                        <h4 className="mb-2">Pull Up, Nov 2020 — Mar 2021</h4>
+                        <h5 className="text-brand-gray-2">Lead Product Designer | Lagos, Nigeria</h5>
                     </div>
 
-                    <div>
-                        <h4>Josplay, Apr 2020 — Jan 2021</h4>
-                        <h5>Product Designer | Remote</h5>
+                    <div className="mb-10">
+                        <h4 className="mb-2">Josplay, Apr 2020 — Jan 2021</h4>
+                        <h5 className="text-brand-gray-2">Product Designer | Remote</h5>
                     </div>
                 </div>
+            </div>
             </div>
         </main>
     )
